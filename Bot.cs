97f3960965f731d4e2d9aff2b0ca21eb4aa3c178
Kaleb Hingsberger <kalebhings@gmail.com>
2026-07-26 15:35:15 -0600

    Add ping slash command to verify everything is working

--- Bot.cs
+++ Bot.cs
@@ -1,29 +1,41 @@
 using Discord;
+using Discord.Rest;
 using Discord.WebSocket;
 
 public class Bot
 {
   private readonly DiscordSocketClient _client;
+  private readonly DiscordRestClient _restClient;
   private readonly string _token;
   private readonly CommandHandler _commandHandler;
+  private readonly string _serverId;
 
-  public Bot(string token)
+  public Bot(string token, string serverId)
   {
     _token = token;
     _client = new DiscordSocketClient();
+    _restClient = new DiscordRestClient();
     _commandHandler = new CommandHandler();
+    _serverId = serverId;
   }
   
   public async Task StartAsync()
   {
     _client.Log += LogAsync;
 
+    _client.InteractionCreated += _commandHandler.HandleCommand;
+
     await _client.LoginAsync(
           TokenType.Bot,
           _token
           );
+    await _restClient.LoginAsync(
+        TokenType.Bot,
+        _token
+        );
     await _client.StartAsync();
-    _client.InteractionCreated += _commandHandler.HandleCommand;
+
+    await RegisterCommandsAsync();
 
     await Task.Delay(-1);
   }
@@ -32,6 +44,18 @@ public class Bot
   {
     Console.WriteLine(message);
     return Task.CompletedTask;
+  }
+
+  private async Task RegisterCommandsAsync()
+  {
+    var command = new SlashCommandBuilder()
+      .WithName("ping")
+      .WithDescription("Replies with pong!");
+
+    await _restClient.CreateGuildCommand(
+        command.Build(),
+        ulong.Parse(_serverId)
+        );
   }
 
 }
--- Program.cs
+++ Program.cs
@@ -6,12 +6,17 @@ public class Program
   {
     Env.Load();
     var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+    var serverId = Environment.GetEnvironmentVariable("DISCORD_SERVER_ID");
     if (string.IsNullOrEmpty(token))
     {
       throw new Exception("Discord token is missing");
     }
+    if (string.IsNullOrEmpty(serverId))
+    {
+      throw new Exception("Discord server ID is missing");
+    }
 
-    var bot = new Bot(token);
+    var bot = new Bot(token, serverId);
 
     await bot.StartAsync();
   }
